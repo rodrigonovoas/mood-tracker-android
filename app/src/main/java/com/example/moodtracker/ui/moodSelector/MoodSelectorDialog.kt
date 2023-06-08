748c773556d0b7e8eaed33be5500bbcc24049776
rodrigonovoasalgado <rodrigonovoa97s@gmail.com>
2023-06-08 19:36:47 +0200

    adding view binding to project and refactoring code

--- app/src/main/java/com/example/moodtracker/ui/moodSelector/MoodSelectorDialog.kt
+++ app/src/main/java/com/example/moodtracker/ui/moodSelector/MoodSelectorDialog.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import com.example.moodtracker.R
 import com.example.moodtracker.data.entity.Mood
+import com.example.moodtracker.databinding.DialogMoodSelectorBinding
 import com.example.moodtracker.db.MoodDatabaseRepository
 import com.example.moodtracker.ui.moodTracker.MoodTrackerActivity
 import com.example.moodtracker.utils.DateUtils
 
 class MoodSelectorDialog: DialogFragment() {
+    private lateinit var binding: DialogMoodSelectorBinding
 
     companion object{
         const val HAPPY_MOOD = 0
@@ -28,7 +30,8 @@ class MoodSelectorDialog: DialogFragment() {
     private lateinit var viewModel: MoodSelectorViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.dialog_mood_selector, container, false)
+        binding = DialogMoodSelectorBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,17 +45,14 @@ class MoodSelectorDialog: DialogFragment() {
 
     private fun setObservers(view: View) {
         viewModel.selectedMoodText.observe(this, Observer { mood ->
-            val tvMood = view.findViewById<TextView>(R.id.tv_mood_description)
-            tvMood.setText(mood)
+            binding.tvMoodDescription.setText(mood)
         })
 
         viewModel.edtCommentVisibility.observe(this, Observer { visible ->
-            val edtComment = view.findViewById<EditText>(R.id.edt_comment)
-
             if (visible) {
-                edtComment.visibility = View.VISIBLE
+                binding.edtComment.visibility = View.VISIBLE
             } else {
-                edtComment.visibility = View.GONE
+                binding.edtComment.visibility = View.GONE
             }
         })
 
@@ -62,23 +62,12 @@ class MoodSelectorDialog: DialogFragment() {
     }
 
     private fun setListeners(view: View) {
-        val imvClose = view.findViewById<ImageView>(R.id.imv_close)
-        imvClose.setOnClickListener { loadMoodsAndDissmiss()}
-
-        val llComment = view.findViewById<LinearLayout>(R.id.ll_comment)
-        llComment.setOnClickListener { viewModel.setCommentVisibility() }
-
-        val imvHappy = view.findViewById<ImageView>(R.id.imv_happy_mood)
-        imvHappy.setOnClickListener { viewModel.setCurrentMood(HAPPY_MOOD) }
-
-        val imvNeutral = view.findViewById<ImageView>(R.id.imv_neutral_mood)
-        imvNeutral.setOnClickListener { viewModel.setCurrentMood(NEUTRAL_MOOD) }
-
-        val imvSad = view.findViewById<ImageView>(R.id.imv_sad_mood)
-        imvSad.setOnClickListener { viewModel.setCurrentMood(SAD_MOOD) }
-
-        val btnContinue = view.findViewById<Button>(R.id.btn_continue)
-        btnContinue.setOnClickListener { addMoodIfSelected(view) }
+        binding.imvClose.setOnClickListener { loadMoodsAndDissmiss()}
+        binding.llComment.setOnClickListener { viewModel.setCommentVisibility() }
+        binding.imvHappyMood.setOnClickListener { viewModel.setCurrentMood(HAPPY_MOOD) }
+        binding.imvNeutralMood.setOnClickListener { viewModel.setCurrentMood(NEUTRAL_MOOD) }
+        binding.imvSadMood.setOnClickListener { viewModel.setCurrentMood(SAD_MOOD) }
+        binding.btnContinue.setOnClickListener { addMoodIfSelected(view) }
     }
 
     private fun addMoodIfSelected(view: View) {
--- app/src/main/java/com/example/moodtracker/ui/moodTracker/MoodTrackerActivity.kt
+++ app/src/main/java/com/example/moodtracker/ui/moodTracker/MoodTrackerActivity.kt
@@ -1,27 +1,27 @@
 package com.example.moodtracker.ui.moodTracker
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.moodtracker.R
 import com.example.moodtracker.data.entity.Mood
+import com.example.moodtracker.databinding.ActivityMoodTrackerBinding
 import com.example.moodtracker.db.MoodDatabaseRepository
 import com.example.moodtracker.ui.moodSelector.MoodListAdapter
 import com.example.moodtracker.ui.moodSelector.MoodSelectorDialog
 import com.example.moodtracker.utils.DateUtils
 
 class MoodTrackerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMoodTrackerBinding
     private lateinit var viewModel: MoodTrackerViewModel
     private lateinit var moodSelectorDialog: MoodSelectorDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mood_tracker)
+        binding = ActivityMoodTrackerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = MoodTrackerViewModel(MoodDatabaseRepository(this))
         moodSelectorDialog = MoodSelectorDialog()
@@ -39,24 +39,23 @@ class MoodTrackerActivity : AppCompatActivity() {
     private fun loadMoodsAndSetLastOne(moods: List<Mood>) {
         if (moods.isEmpty()) return
 
-        val rcMoods = findViewById<RecyclerView>(R.id.rc_mood)
         val adapter = MoodListAdapter(moods)
 
         adapter.onMoodClick = {
             setMoodDataInScreen(it)
         }
 
-        rcMoods.layoutManager = LinearLayoutManager(
+        binding.rcMood.layoutManager = LinearLayoutManager(
             this,
             LinearLayoutManager.HORIZONTAL,
             false
         )
 
-        rcMoods.adapter = adapter
+        binding.rcMood.adapter = adapter
 
         // scroll to last mood and set its data
         val lastPosition = moods.size - 1
-        rcMoods.scrollToPosition(lastPosition)
+        binding.rcMood.scrollToPosition(lastPosition)
         setMoodDataInScreen(moods[lastPosition])
     }
 
@@ -74,39 +73,27 @@ class MoodTrackerActivity : AppCompatActivity() {
     }
 
     private fun setHappyMoodScreen(mood: Mood) {
-        val tvComment = findViewById<TextView>(R.id.tv_mood_comment)
-        val tvDate = findViewById<TextView>(R.id.tv_mood_date)
-        val imvMood = findViewById<ImageView>(R.id.imv_mood_status)
-        val screenBackground = findViewById<ConstraintLayout>(R.id.main_view)
-
-        tvComment.setText("\"" + mood.comment + "\"")
-        tvDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
-        imvMood.setImageDrawable(getDrawable(R.drawable.ic_happy_mood))
-        screenBackground.setBackgroundColor(getColor(R.color.happiness_status))
+        binding.tvMoodComment.text = ""
+        if (mood.comment != null && mood.comment.isNotEmpty()) binding.tvMoodComment.setText("\"" + mood.comment + "\"")
+        binding.tvMoodDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
+        binding.imvMoodStatus.setImageDrawable(getDrawable(R.drawable.ic_happy_mood))
+        binding.mainView.setBackgroundColor(getColor(R.color.happiness_status))
     }
 
     private fun setNeutralMoodScreen(mood: Mood) {
-        val tvComment = findViewById<TextView>(R.id.tv_mood_comment)
-        val tvDate = findViewById<TextView>(R.id.tv_mood_date)
-        val imvMood = findViewById<ImageView>(R.id.imv_mood_status)
-        val screenBackground = findViewById<ConstraintLayout>(R.id.main_view)
-
-        tvComment.setText("\"" + mood.comment + "\"")
-        tvDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
-        imvMood.setImageDrawable(getDrawable(R.drawable.ic_neutral_mood))
-        screenBackground.setBackgroundColor(getColor(R.color.neutral_status))
+        binding.tvMoodComment.text = ""
+        if (mood.comment != null && mood.comment.isNotEmpty()) binding.tvMoodComment.setText("\"" + mood.comment + "\"")
+        binding.tvMoodDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
+        binding.imvMoodStatus.setImageDrawable(getDrawable(R.drawable.ic_neutral_mood))
+        binding.mainView.setBackgroundColor(getColor(R.color.neutral_status))
     }
 
     private fun setSadMoodScreen(mood: Mood) {
-        val tvComment = findViewById<TextView>(R.id.tv_mood_comment)
-        val tvDate = findViewById<TextView>(R.id.tv_mood_date)
-        val imvMood = findViewById<ImageView>(R.id.imv_mood_status)
-        val screenBackground = findViewById<ConstraintLayout>(R.id.main_view)
-
-        tvComment.setText("\"" + mood.comment + "\"")
-        tvDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
-        imvMood.setImageDrawable(getDrawable(R.drawable.ic_sad_mood))
-        screenBackground.setBackgroundColor(getColor(R.color.sad_status))
+        binding.tvMoodComment.text = ""
+        if (mood.comment != null && mood.comment.isNotEmpty()) binding.tvMoodComment.setText("\"" + mood.comment + "\"")
+        binding.tvMoodDate.setText(DateUtils.convertLongToLongDate(mood.creationDate))
+        binding.imvMoodStatus.setImageDrawable(getDrawable(R.drawable.ic_sad_mood))
+        binding.mainView.setBackgroundColor(getColor(R.color.sad_status))
     }
 
     fun getMoods() {
